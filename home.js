5083245e6c03a51a494ceccf9c07014c60e71e81
Alinemati1987 <89942725+Alinemati1987@users.noreply.github.com>
2023-05-19 07:59:36 +0200

    Seconf  part of exercise3 is done

--- home.js
+++ home.js
@@ -13,25 +13,64 @@ const lines = input.trim().split("\n");
 const priority = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let prioritySum = 0;
 
-lines.forEach((line) => {
-  const lineLength = line.length;
-  let partLength = Math.floor(lineLength / 2);
+// partOne();
+partTwo();
 
-  let partOne = line.slice(0, partLength);
-  let partTwo = line.slice(partLength);
+function partOne() {
+  lines.forEach((line) => {
+    const lineLength = line.length;
+    let partLength = Math.floor(lineLength / 2);
 
-  let commonCharecter = [];
+    let partOne = line.slice(0, partLength);
+    let partTwo = line.slice(partLength);
 
-  for (let char of partOne) {
-    if (partTwo.includes(char) && !commonCharecter.includes(char)) {
-      commonCharecter.push(char);
+    let commonCharecter = [];
+
+    for (let char of partOne) {
+      if (partTwo.includes(char) && !commonCharecter.includes(char)) {
+        commonCharecter.push(char);
+      }
     }
+
+    commonCharecter.forEach((character) => {
+      let location = priority.indexOf(character);
+      prioritySum += location + 1;
+    });
+  });
+
+  console.log("The priorities sum is: " + prioritySum);
+}
+
+function partTwo() {
+  var groups = [];
+
+  for (let i = 0; i < lines.length; i += 3) {
+    let slices = lines.slice(i, i + 3);
+    groups.push(slices);
   }
 
-  commonCharecter.forEach((character) => {
-    let location = priority.indexOf(character);
-    prioritySum += location + 1;
-  });
-});
+  groups.forEach((group) => {
+    let commonCharecter = [];
 
-console.log("The priorities sum is: " + prioritySum);
+    let lineOne = group[0];
+    let lineTwo = group[1];
+    let lineThree = group[2];
+
+    for (let char of lineOne) {
+      if (
+        lineTwo.includes(char) &&
+        lineThree.includes(char) &&
+        !commonCharecter.includes(char)
+      ) {
+        commonCharecter.push(char);
+      }
+    }
+
+    commonCharecter.forEach((character) => {
+      let location = priority.indexOf(character);
+      prioritySum += location + 1;
+    });
+  });
+
+  console.log("The priorities sum is: " + prioritySum);
+}
